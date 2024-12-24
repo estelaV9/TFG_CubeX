fdbf42c9eac11a23ef6842e266185a9ab0bbc65a
estelaV9 <esteladeveg@gmail.com>
2024-12-24 17:53:50 +0100

    crear una clase con los colores que va a tener la aplicacion

---
+++ lib/color/app_color.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color purpleIntroColor = Color(0xFFa88ee7);
+  static const Color titlePurple = Color(0xFF8112DD);
+} // CLASE CON ATRIBUTOS DE LOS COLORES DE LA APP
