2586643548a37c887247339d8444755e51146cbf
estelaV9 <esteladeveg@gmail.com>
2024-12-27 19:26:12 +0100

    formateo de lineas, añadir un formulario con los distintos fieldForm, controllers y validators y crear el metodo para hacer el registro

--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -1,6 +1,16 @@
-import 'package:esteladevega_tfg_cubex/color/app_color.dart';
+import 'package:esteladevega_tfg_cubex/dao/user_dao.dart';
+import 'package:esteladevega_tfg_cubex/database/database_helper.dart';
+import 'package:esteladevega_tfg_cubex/main.dart';
+import 'package:esteladevega_tfg_cubex/model/user.dart';
+import 'package:esteladevega_tfg_cubex/utilities/alert.dart';
+import 'package:esteladevega_tfg_cubex/utilities/app_color.dart';
 import 'package:esteladevega_tfg_cubex/components/icon_image_fieldrow.dart';
+import 'package:esteladevega_tfg_cubex/utilities/change_screen.dart';
+import 'package:esteladevega_tfg_cubex/utilities/encrypt_password.dart';
+import 'package:esteladevega_tfg_cubex/utilities/validator.dart';
 import 'package:flutter/material.dart';
+
+import '../components/password_field_row.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -10,6 +20,40 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  UserDao userDao = UserDao();
+  String _password = ''; // ATRIBUTO PARA GUARDAR LA CONTRASEÑA
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  Future<void> _signUp() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      final username = _usernameController.text;
+      final mail = _mailController.text;
+      String encryptedPassword = EncryptPassword.encryptPassword(_password);
+      DatabaseHelper.logger.i('Encrypted password: $encryptedPassword');
+
+      // SE CREA UN USUARIO
+      User user =
+          User(username: username, mail: mail, password: encryptedPassword);
+
+      if (await userDao.insertUser(user)) {
+        // SE MUESTRA UN SNACKBAR DE QUE SE HA CREADO CORRECTAMENTE Y SE REDIRIGE A LA PANTALLA PRINCIPAL
+        AlertUtil.showSnackBar(context, Icons.info,
+            "Account created\nsuccessfully.", Colors.green);
+        ChangeScreen.changeScreen(IntroScreen(), context);
+      } else {
+        // SE MUESTRA UN SNACKBARR MOSTRANDO QUE HA OCURRIDO UN ERRO AL CREAR USUARIO
+        AlertUtil.showSnackBar(context, Icons.error,
+            "An error occurred while\ncreating the account.", Colors.redAccent);
+      } // INSERTAR AL USUARIO
+    } // SI TODOS LOS CAMPOS DEL FORMULARIO ESTAN CORRECTOS
+  } // METODO PARA CREAR UNA CUENTA
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,44 +93,59 @@ class _SignUpScreenState extends State<SignUpScreen> {
                             fontSize: 70,
                             color: AppColors.lightPurpleColor),
                       ),
-
                       const SizedBox(height: 10),
+                      Form(
+                          key: _formKey,
+                          child: Column(children: [
+                            FieldForm(
+                              icon: const Icon(Icons.person),
+                              labelText: 'Username',
+                              hintText: 'Write your username',
+                              controller: _usernameController,
+                              validator: (value) =>
+                                  Validator.validateUsername(value),
+                            ),
 
-                      /*FieldForm(
-                        icon: const Icon(Icons.person),
-                        labelText: 'Username',
-                        hintText: 'Write your username',
-                      ),
+                            // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
+                            const SizedBox(height: 10),
 
-                      // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
-                      const SizedBox(height: 10),
+                            FieldForm(
+                              icon: const Icon(Icons.mail),
+                              labelText: 'Mail',
+                              hintText: 'Write your mail',
+                              controller: _mailController,
+                              validator: (value) =>
+                                  Validator.validateEmail(value),
+                            ),
 
-                      FieldForm(
-                        icon: const Icon(Icons.mail),
-                        labelText: 'Mail',
-                        hintText: 'Write your mail',
-                      ),
+                            // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
+                            const SizedBox(height: 10),
 
-                      // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
-                      const SizedBox(height: 10),
+                            PasswordFieldForm(
+                              icon: const Icon(Icons.lock),
+                              labelText: 'Password',
+                              hintText: 'Write your password',
+                              controller: _passwordController,
+                              validator: (value) =>
+                                  Validator.validatePassword(value),
+                              passwordOnSaved: (value) => _password = value!,
+                            ),
 
-                      FieldForm(
-                        icon: const Icon(Icons.lock),
-                        labelText: 'Password',
-                        hintText: 'Write your password',
-                      ),
+                            // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
+                            const SizedBox(height: 10),
 
-                      // ESPACIO ENTRE LOS CAMPOS DEL FORMULARIO
-                      const SizedBox(height: 10),
-
-                      FieldForm(
-                        icon: const Icon(Icons.check),
-                        labelText: 'Confirm password',
-                        hintText: 'Confirm password',
-                      ),*/
-
-                      const SizedBox(height: 30),
-
+                            PasswordFieldForm(
+                              icon: const Icon(Icons.check),
+                              labelText: 'Confirm password',
+                              hintText: 'Confirm password',
+                              controller: _confirmPasswordController,
+                              validator: (value) =>
+                                  Validator.validateConfirmPassword(
+                                      value, _passwordController.text),
+                              passwordOnSaved: (value) => _password = value!,
+                            ),
+                          ])),
+                      const SizedBox(height: 15),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -99,16 +158,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           ),
                           const SizedBox(width: 14),
                           ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                _signUp();
+                              },
                               style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets
-                                    .zero, // LE QUITAMOS EL PADDING DE DENTRO DEL BTON
+                                // LE QUITAMOS EL PADDING DE DENTRO DEL BTON
+                                padding: EdgeInsets.zero,
                               ),
                               child: const Icon(Icons.arrow_forward)),
                         ],
                       ),
-                      const SizedBox(height: 30),
-
+                      const SizedBox(height: 15),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
