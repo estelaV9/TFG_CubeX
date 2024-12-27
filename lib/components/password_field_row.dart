2591880ab0b6e50a73e42c644931ae90f6d7d475
estelaV9 <esteladeveg@gmail.com>
2024-12-27 19:22:38 +0100

    hacer un formateo de lineas a la clase PasswordFieldForm y añadirle un parametro para guardar la contraseña

--- lib/components/password_field_row.dart
+++ lib/components/password_field_row.dart
@@ -1,4 +1,4 @@
-import 'package:esteladevega_tfg_cubex/color/app_color.dart';
+import 'package:esteladevega_tfg_cubex/utilities/app_color.dart';
 import 'package:flutter/material.dart';
 
 class PasswordFieldForm extends StatefulWidget {
@@ -7,14 +7,15 @@ class PasswordFieldForm extends StatefulWidget {
   final String hintText; // TEXTO QUE VA A APARECER CUANDO SE PULSA
   final TextEditingController controller; // PASAMOS TAMBIEN EL CONTROLADOR
   final FormFieldValidator<String> validator; // PASAMOS LA VALIDACION
+  final FormFieldValidator<String> passwordOnSaved;
 
-  const PasswordFieldForm(
-      {super.key,
-      required this.icon,
-      required this.labelText,
-      required this.hintText,
-      required this.controller,
-      required this.validator});
+  const PasswordFieldForm({super.key,
+    required this.icon,
+    required this.labelText,
+    required this.hintText,
+    required this.controller,
+    required this.validator,
+    required this.passwordOnSaved});
 
   @override
   _PasswordFieldFormState createState() => _PasswordFieldFormState();
@@ -30,60 +31,61 @@ class _PasswordFieldFormState extends State<PasswordFieldForm> {
       children: [
         Expanded(
             child: Container(
-          height: 62,
-          decoration: BoxDecoration(
-            color: AppColors.lightVioletColor,
-            borderRadius: BorderRadius.circular(100),
+              height: 62,
+              decoration: BoxDecoration(
+                color: AppColors.lightVioletColor,
+                borderRadius: BorderRadius.circular(100),
 
-            // AÑADIMOS EL EFECTO DE "drop shadow"
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2), // COLOR DE LA SOMBRA
-                spreadRadius: 2, // LARGURA DE LA SOMBRA
-                blurRadius: 5, // EFECTO BLUR DE LA SOMBRA
-                // DONDE SE VA A COLOCAR HORIZONTAL Y VERTICALMENTE
-                offset: const Offset(2, 4),
+                // AÑADIMOS EL EFECTO DE "drop shadow"
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2), // COLOR DE LA SOMBRA
+                    spreadRadius: 2, // LARGURA DE LA SOMBRA
+                    blurRadius: 5, // EFECTO BLUR DE LA SOMBRA
+                    // DONDE SE VA A COLOCAR HORIZONTAL Y VERTICALMENTE
+                    offset: const Offset(2, 4),
+                  ),
+                ],
               ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                widget.icon,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    widget.icon,
 
-                // ESPACIO ENTRE EL ICONO Y EL FORMULARIO
-                const SizedBox(width: 20),
+                    // ESPACIO ENTRE EL ICONO Y EL FORMULARIO
+                    const SizedBox(width: 20),
 
-                Expanded(
-                  child: TextFormField(
-                      controller: widget.controller,
-                      obscureText: _isObscure,
-                      decoration: InputDecoration(
-                        labelText: widget.labelText,
-                        hintText: widget.hintText,
-                      ),
-                      validator: widget.validator),
+                    Expanded(
+                      child: TextFormField(
+                        controller: widget.controller,
+                        obscureText: _isObscure,
+                        decoration: InputDecoration(
+                          labelText: widget.labelText,
+                          hintText: widget.hintText,
+                        ),
+                        validator: widget.validator,
+                        onSaved: widget.passwordOnSaved),
+                    ),
+
+                    IconButton(
+                        onPressed: () {
+                          // CUANDO PRESIONE EL BOTON DE VER SE CAMBIARA EL ESTADO DEL ATRIBUTO "isObscure"
+                          // DE ESTA FORMA CUANDO PULSE SE MOSTRARA/OCULTARA LA CONTRASEÑA
+                          setState(() {
+                            if (_isObscure) {
+                              _isObscure = true; // SE CAMBIA A TRUE
+                            } else {
+                              _isObscure = false; // SE CAMBIA A FALSE
+                            }
+                            _isObscure = !_isObscure; // SE CAMBIA EL ESTADO
+                          });
+                        },
+                        icon: const Icon(Icons.remove_red_eye))
+                  ],
                 ),
-
-                IconButton(
-                    onPressed: () {
-                      // CUANDO PRESIONE EL BOTON DE VER SE CAMBIARA EL ESTADO DEL ATRIBUTO "isObscure"
-                      // DE ESTA FORMA CUANDO PULSE SE MOSTRARA/OCULTARA LA CONTRASEÑA
-                      setState(() {
-                        if (_isObscure) {
-                          _isObscure = true; // SE CAMBIA A TRUE
-                        } else {
-                          _isObscure = false; // SE CAMBIA A FALSE
-                        }
-                        _isObscure = !_isObscure; // SE CAMBIA EL ESTADO
-                      });
-                    },
-                    icon: const Icon(Icons.remove_red_eye))
-              ],
-            ),
-          ),
-        ))
+              ),
+            ))
       ],
     );
   }
