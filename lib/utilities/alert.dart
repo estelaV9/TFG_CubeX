6505fce17477f0079d5a4a0d78e0b9b8c3330fa4
estelaV9 <esteladeveg@gmail.com>
2024-12-27 19:12:40 +0100

    crear un metodo para mostrar un snackbarr

--- lib/utilities/alert.dart
+++ lib/utilities/alert.dart
@@ -11,7 +11,8 @@ class AlertUtil {
             actions: [
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(); // SE CIERRA EL DIALOGO CUANDO PULSE "aceptar"
+                  // SE CIERRA EL DIALOGO CUANDO PULSE "aceptar"
+                  Navigator.of(context).pop();
                 },
                 child: const Text("Accept"),
               ),
@@ -19,4 +20,32 @@ class AlertUtil {
           );
         });
   } // METODO PARA MOSTRAR UNA ALERTA
+
+  static showSnackBar(
+      BuildContext context, IconData icon, String message, Color color) {
+    // CREAMOS EL SNACKBAR
+    final snackBar = SnackBar(
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: color, // COLOR DE FONDO
+      action: SnackBarAction(
+        label: "Accept", // TEXTO DEL BOTON DE ACCION
+        textColor: Colors.yellow, // COLOR DEL TEXTO DEL BOTON
+        onPressed: () {
+          // SE CIERRA EL SNACKBAR CUANDO PULSE "Accept"
+          Navigator.of(context).pop();
+        },
+      ),
+      content: Row(
+        children: [Icon(icon), const SizedBox(width: 10), Text(message)],
+      ), // EL TEXTO QUE SE MOSTRARA EN EL SNACKBAR
+    );
+
+    // MOSTRAMOS EL SNACKBAR
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+    // SE CIERRA EL SNACKBAR PASADOS LOS 5 SEGUNDOS
+    Future.delayed(const Duration(seconds: 5), () {
+      ScaffoldMessenger.of(context).clearSnackBars();
+    });
+  } // MOSTRAR UN SNACKBAR
 }
