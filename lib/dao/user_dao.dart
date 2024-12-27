0d4b68280e3538450f616eabdabfca592c9d2c79
estelaV9 <esteladeveg@gmail.com>
2024-12-27 19:20:19 +0100

    crear el metodo para insertar usuario

--- lib/dao/user_dao.dart
+++ lib/dao/user_dao.dart
@@ -1,5 +1,6 @@
-import 'package:esteladevega_tfg_cubex/utilities/alert.dart';
+import 'package:esteladevega_tfg_cubex/model/user.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
 
 import '../database/database_helper.dart';
 
@@ -19,4 +20,23 @@ class UserDao {
       return false;
     }
   } // METODO PARA VALIDAR EL LOGIN DEL USUARIO CON EL NOMBRE O MAIL DE USUARIO
+
+  Future<bool> insertUser(User user) async {
+    final db = await DatabaseHelper.database;
+    try {
+      await db.insert('user', {
+        'username': user.username,
+        'mail': user.mail,
+        'passwordHash': user.password,
+        'creationDate': user.creationDate,
+        'imageUrl': user.imageUrl,
+      }); // SE INSERTA EL USUARIO
+
+      DatabaseHelper.logger.i(user.toString());
+      return true;
+    } catch (e) {
+      DatabaseHelper.logger.w("Error al crear usuario: $e");
+      return false;
+    } // try-catch
+  } // METODO PARA INSERTAR UN USUARIO
 }
