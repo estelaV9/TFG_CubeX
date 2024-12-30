ce7ad4657a2653c92a7f4b86db4195f3e7063287
estelaV9 <esteladeveg@gmail.com>
2024-12-30 15:34:24 +0100

    modificar un poco el codigo anteriormente realizado y añadir el espacio del timer y de las estadisticas

--- lib/screen/timer_screen.dart
+++ lib/screen/timer_screen.dart
@@ -12,6 +12,15 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
+  var averageValue = "--:--.--";
+  var pbValue = "--:--.--";
+  var worstValue = "--:--.--";
+  var countValue = "--:--.--";
+  var ao5Value = "--:--.--";
+  var ao12Value = "--:--.--";
+  var ao50Value = "--:--.--";
+  var ao100Value = "--:--.--";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,34 +44,169 @@ class _TimerScreenState extends State<TimerScreen> {
             ),
           )),
 
-          // BLOQUE PARA LOS AJUSTES Y EL TIPO DE CUBO Y LA SESION
-          const Stack(
-            children: [
-              // BOTON DE CONFIGURACION ARRIBA A LA IZQUIERDA
-              Positioned(
-                top: 20,
-                left: 20,
-                child: Icon(
-                  Icons.settings,
-                  size: 30,
-                  color: AppColors.darkPurpleColor,
+          // BOTON DE CONFIGURACION ARRIBA A LA IZQUIERDA
+          const Positioned(
+            top: 20,
+            left: 20,
+            child: Icon(
+              Icons.settings,
+              size: 30,
+              color: AppColors.darkPurpleColor,
+            ),
+          ),
+
+          // CONTAINER DEL TIPO DE CUBO Y LA SESION UN POCO MAS ABAJO A LA DERECHA
+          const Positioned(
+            top: 40,
+            right: 20,
+            child: CubeHeaderContainer(),
+          ),
+
+          const Positioned(
+            top: 110,
+            right: 20,
+            left: 20,
+            child: ScrambleContainer(),
+          ),
+
+          // .fill PARA QUE SE EXPANDA EL TIMER Y SIGA QUEDANDOSE EN EL CENTRO
+          Positioned.fill(
+            top: 270, // PARA QUE SE COLOQUE JUSTO DESPUES DEL SCRAMBLE
+            child: Column(
+              children: [
+                Container(
+                  padding:
+                      // TODO_EL ESPACIO QUE OCUPA EL ESPACIO DEL TIMER
+                      const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "0.00",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.darkPurpleColor,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.add_comment_rounded,
+                                  color: AppColors.darkPurpleColor)),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "DNF",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor,
+                                ),
+                              )),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "+2",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor,
+                                ),
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.close,
+                                  color: AppColors.darkPurpleColor)),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
 
-              // CONTAINER DEL TIPO DE CUBO Y LA SESION UN POCO MAS ABAJO A LA DERECHA
-              Positioned(
-                top: 40,
-                right: 20,
-                child: CubeHeaderContainer(),
-              ),
+                // EXPANDE EL CONTENEDOR CON LOS TEXTOS PARA QUE OCUPE TODO_EL ESPACIO
+                // DISPONIBLE ENTRE EL TIMER Y LOS TEXTOS
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      // ESPACIO ENTRE LAS COLUMNAS
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // COLUMNA IZQUIERDA
+                        Column(
+                          // EMPIEZA DESDE ARRIBA A LA IZQUIERDA
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Average: $averageValue",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Pb: $pbValue",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Worst: $worstValue",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Count: $countValue",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                          ],
+                        ),
 
-              Positioned(
-                top: 110,
-                right: 20,
-                left: 20,
-                child: ScrambleContainer(),
-              ),
-            ],
+                        //COLUMNA DERECHA
+                        Column(
+                          // EMPIEZA DESDE ARRIBA A LA DERECHA
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Ao5: $ao5Value",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Ao12: $ao12Value",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Ao50: $ao50Value",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                            Text(
+                              "Ao100: $ao100Value",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.darkPurpleColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
