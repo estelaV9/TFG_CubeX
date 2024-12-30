3cfd96ec2a4022583fd7c6dc1b1c98ca93a176bd
estelaV9 <esteladeveg@gmail.com>
2024-12-30 13:13:06 +0100

    modificar la clase timerScreen y añadir el boton de settings, el container del tipo de cubo y el del scramble

--- lib/screen/timer_screen.dart
+++ lib/screen/timer_screen.dart
@@ -1,3 +1,5 @@
+import 'package:esteladevega_tfg_cubex/components/cube_header_container.dart';
+import 'package:esteladevega_tfg_cubex/components/scramble_container.dart';
 import 'package:flutter/material.dart';
 
 import '../utilities/app_color.dart';
@@ -13,24 +15,57 @@ class _TimerScreenState extends State<TimerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
+      body: Stack(
+        children: [
+          // FONDO DEGRADADO
+          Positioned.fill(
+              child: Container(
+            decoration: const BoxDecoration(
+              // COLOR DEGRADADO PARA EL FONDO
+              gradient: LinearGradient(
+                begin: Alignment.topCenter, // DESDE ARRIBA
+                end: Alignment.bottomCenter, // HASTA ABAJO
+                colors: [
+                  // COLOR DE ARRIBA DEL DEGRADADO
+                  AppColors.upLinearColor,
+                  // COLOR DE ABAJO DEL DEGRADADO
+                  AppColors.downLinearColor,
+                ],
+              ),
+            ),
+          )),
+
+          // BLOQUE PARA LOS AJUSTES Y EL TIPO DE CUBO Y LA SESION
+          const Stack(
             children: [
-              Positioned.fill(
-                  child: Container(
-                decoration: const BoxDecoration(
-                  // COLOR DEGRADADO PARA EL FONDO
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter, // DESDE ARRIBA
-                    end: Alignment.bottomCenter, // HASTA ABAJO
-                    colors: [
-                      AppColors.upLinearColor,
-                      // COLOR DE ARRIBA DEL DEGRADADO
-                      AppColors.downLinearColor,
-                      // COLOR DE ABAJO DEL DEGRADADO
-                    ],
-                  ),
+              // BOTON DE CONFIGURACION ARRIBA A LA IZQUIERDA
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Icon(
+                  Icons.settings,
+                  size: 30,
+                  color: AppColors.darkPurpleColor,
                 ),
-              )),
-    ]));
+              ),
+
+              // CONTAINER DEL TIPO DE CUBO Y LA SESION UN POCO MAS ABAJO A LA DERECHA
+              Positioned(
+                top: 40,
+                right: 20,
+                child: CubeHeaderContainer(),
+              ),
+
+              Positioned(
+                top: 110,
+                right: 20,
+                left: 20,
+                child: ScrambleContainer(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
