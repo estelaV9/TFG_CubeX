e4bd7f336994d0bd7340f4c05bfc23a38c31aaa9
estelaV9 <esteladeveg@gmail.com>
2024-12-30 18:26:41 +0100

    Añadir mas colores a la aplicacion

--- lib/utilities/app_color.dart
+++ lib/utilities/app_color.dart
@@ -8,4 +8,7 @@ class AppColors {
   static const Color lightPurpleColor = Color(0xFF8E4EE0);
   static const Color lightVioletColor = Color(0xFFC5B7E7);
   static const Color darkPurpleColor = Color(0xFF41135E);
+  static const Color purpleA172de = Color(0xFFa172de);
+  static const Color imagenBg = Color(0xFFa384bd);
+  static const Color listTileHover = Color(0xFFc5b7e7);
 } // CLASE CON ATRIBUTOS DE LOS COLORES DE LA APP
