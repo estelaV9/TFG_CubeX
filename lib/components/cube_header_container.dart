abf481a71e3df7d4948b68caf0018b9386b32cb4
estelaV9 <esteladeveg@gmail.com>
2024-12-31 14:53:33 +0100

    Modificar la cabecera de tipo de cubo y sesion para añadir el overlay para que se muestre el menu de sesion

--- lib/components/cube_header_container.dart
+++ lib/components/cube_header_container.dart
@@ -1,3 +1,4 @@
+import 'package:esteladevega_tfg_cubex/components/session_menu.dart';
 import 'package:esteladevega_tfg_cubex/utilities/app_color.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,54 @@ class CubeHeaderContainer extends StatefulWidget {
 }
 
 class _CubeHeaderContainerState extends State<CubeHeaderContainer> {
+  // SE AÑADE EN ESTA CLASE EL OVERLAY DEL MENU SESSION
+  bool isMenuVisible = false; // COMPROBAR SI EL MENU ESTA VISIBLE O NO
+  OverlayEntry? _overlayEntry; // MANEJAR EL MENU COMO OVERLAY
+
+  void _showOverlay() {
+    _overlayEntry = OverlayEntry(
+      builder: (context) {
+        return GestureDetector(
+          onTap: () {
+            // AL TOCAR FUERA, SE CIERRA EL MENU
+            _removeOverlay(); // ELIMINAR EL OVERLAY
+          },
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Container(
+                  // SE PONE EL FONDO MEDIO OSCURO CUANDO APARECE EL MENU
+                  color: Colors.black.withOpacity(0.4),
+                ),
+              ),
+              Center(
+                // ESTE CONTAINER SIRVE PARA CENTRARLO EN LA PANTALLA EL OTRO CONTAINER
+                child: Container(
+                  width: 250,
+                  height: 300,
+                  child: const SessionMenu(), // SESSION MENU
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    // SE INSERTA EL OVERLAY EN LA PANTALLA
+    Overlay.of(context)?.insert(_overlayEntry!);
+    setState(() {
+      isMenuVisible = true; // SE HACE VISIBLE EL MENU
+    });
+  } // METODO PARA AÑADIR EL OVERLAY
+
+  void _removeOverlay() {
+    _overlayEntry?.remove(); // SE ELIMINA EL OVERLAY
+    setState(() {
+      isMenuVisible = false;
+    }); // SE ACTUALIZA EL ESTADO VISIBLE
+  } // METODO PARA ELIMINAR EL OVERLAY
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -43,7 +92,14 @@ class _CubeHeaderContainerState extends State<CubeHeaderContainer> {
             const SizedBox(width: 10),
 
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  // CUANDO SE TOCA, SE MUESTRA/OCULTA EL MENU DE SESSION
+                  if (isMenuVisible) {
+                    _removeOverlay(); // SI EL MENU ESTA VISIBLE, SE OCULTA
+                  } else {
+                    _showOverlay(); // SI NO ESTA VISIBLE, SE MUESTRA
+                  }
+                },
                 icon: const Icon(
                   Icons.square,
                   color: AppColors.darkPurpleColor,
