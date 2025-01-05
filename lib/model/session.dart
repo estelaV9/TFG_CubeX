7357f97091b08f167424599bbdb69d8fda884e47
estelaV9 <esteladeveg@gmail.com>
2025-01-05 19:00:14 +0100

    Crear la clase sesion con sus atributos, constructores y el metodo toString

---
+++ lib/model/session.dart
@@ -0,0 +1,33 @@
+class Session {
+  int? idSession;
+  int idUser;
+  String sessionName;
+  String creationDate;
+  int idCubeType;
+
+  Session({
+    int? idSession,
+    required this.idUser,
+    required this.sessionName,
+    String? creationDate,
+    required this.idCubeType,
+  }) : creationDate = creationDate ?? DateTime.now().toString();
+
+  // CONSTRUCTOR VACIO
+  Session.empty()
+      : idSession = null,
+        idUser = 0,
+        sessionName = '',
+        creationDate = DateTime.now().toString(),
+        idCubeType = 0;
+
+  @override
+  String toString() {
+    return 'Session{'
+        'idSession: $idSession, '
+        'idUser: $idUser, '
+        'sessionName: $sessionName, '
+        'creationDate: $creationDate, '
+        'idCubeType: $idCubeType}';
+  }
+}
