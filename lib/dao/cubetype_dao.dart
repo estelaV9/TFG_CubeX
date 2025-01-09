a4a715f808e4bc9ff04d1f21a19427d788c69b5d
estelaV9 <esteladeveg@gmail.com>
2025-01-09 23:35:56 +0100

    Crear los metodos para buscar por nombre de tipo de cubo e insertar uno nuevo

--- lib/dao/cubetype_dao.dart
+++ lib/dao/cubetype_dao.dart
@@ -8,7 +8,10 @@ class CubeTypeDao {
       // CONSULTA PARA OBTENER TODOS LOS TIPOS DE CUBOS
       final result = await db.query('cubeType');
       // MAPEAR LOS RESULTADOS A UNA LISTA DE OBJETOS CubeType
-      return result.map((map) => CubeType(map['idCubeType'] as int, map['cubeName'] as String)).toList();
+      return result
+          .map((map) =>
+              CubeType(map['idCubeType'] as int, map['cubeName'] as String))
+          .toList();
     } catch (e) {
       DatabaseHelper.logger.e("Error al obtener los tipos de cubos: $e");
       return []; // RETORNA UNA LISTA VACÍA EN CASO DE ERROR
@@ -27,7 +30,8 @@ class CubeTypeDao {
 
       if (result.isNotEmpty) {
         // CONVIERTE EL PRIMER RESULTADO EN UN OBJETO
-        return CubeType(result.first['idCubeType'] as int, result.first['cubeName'] as String);
+        return CubeType(result.first['idCubeType'] as int,
+            result.first['cubeName'] as String);
       } else {
         // SI NO ENCUENTRA EL CUBO, DEVUELVE UN TIPO DE CUBO DE EROOR
         return CubeType(-1, "ErrorCube");
@@ -39,4 +43,43 @@ class CubeTypeDao {
       return CubeType(-1, "ErrorCube");
     }
   } // METODO PARA BUSCAR UN TIPO DE CUBO POR SU NOMBER
+
+  Future<bool> isExistsCubeTypeName(String name) async {
+    final db = await DatabaseHelper.database;
+    try {
+      // REALIZA LA CONSULTA A LA BASE DE DATOS
+      final result = await db.query(
+        'cubeType',
+        where: 'cubeName = ?',
+        whereArgs: [name],
+      );
+
+      if (result.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      } // SI EL RESULTADO ESTA O NO VACIO, DEVOLVERA TRUE/FALSE
+    } catch (e) {
+      DatabaseHelper.logger
+          .e("Error al obtener el tipo de cubo por defecto: $e");
+      // RETORNA FALSE EN CASO DE ERROR
+      return false;
+    }
+  } // METODO PARA SABER SI EL NOMBRE DE UN TIPO DE CUBO YA EXISTE
+
+  Future<bool> insertNewType(String name) async {
+    final db = await DatabaseHelper.database;
+    try {
+      final result = await db.insert('cubeType', {'cubeName': name});
+
+      if (result > 0) {
+        return true; // SE INSERTO CORRECTAMENTE
+      } else {
+        return false; // NO SE INSERTO CORRECTAMENTE
+      } // VERIFICAMOS SI SE HA INSERTADO CORRECTAMENTE
+    } catch (e) {
+      DatabaseHelper.logger.e("Error al insertar un nuevo tipo de cubo: $e");
+      return false; // DEVUELVE FALSE EN CASO DE ERROR
+    }
+  } // METODO PARA INSERTAR UN NUEVO TIPO DE CUBO
 }
