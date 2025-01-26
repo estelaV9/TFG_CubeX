e83effc364de3c461f1b19e498c0e236d8ee640f
estelaV9 <esteladeveg@gmail.com>
2025-01-26 18:01:17 +0100

    quitar el overlay y poner un modelBottomSheet para mostrar el container de tipos de cubos

--- lib/components/Icon/animated_icon.dart
+++ lib/components/Icon/animated_icon.dart
@@ -5,7 +5,11 @@ import 'package:flutter/material.dart';
 class AnimatedIconWidget extends StatefulWidget {
   final AnimatedIconData animatedIconData;
 
-  const AnimatedIconWidget({super.key, required this.animatedIconData});
+  const AnimatedIconWidget({
+    super.key,
+    required this.animatedIconData,
+    required Null Function() onPressed,
+  });
 
   @override
   _AnimatedIconWidgetState createState() => _AnimatedIconWidgetState();
@@ -15,7 +19,6 @@ class _AnimatedIconWidgetState extends State<AnimatedIconWidget>
     with SingleTickerProviderStateMixin {
   bool isMenuVisible = false; // COMPROBAR SI EL MENU ESTA VISIBLE O NO
   late AnimationController animationController; // CONTROLADOR DE LA ANIMACIÓN
-  OverlayEntry? _overlayEntry; // MANEJAR EL MENU COMO OVERLAY
 
   @override
   void initState() {
@@ -40,69 +43,40 @@ class _AnimatedIconWidgetState extends State<AnimatedIconWidget>
       ),
       onPressed: () {
         if (isMenuVisible) {
-          // SI EL MENU ESTA VISIBLE, SE OCULTA
-          _removeOverlay(); // SE ELIMINA EL OVERLAY
+          // SI EL MENU ESTA VISIBLE, SE EL MODAL
+          Navigator.of(context).pop();
           animationController.reverse();
         } else {
           // SI EL MENU NO ESTA VISIBLE, SE MUESTRA
           _showOverlay(); // MOSTRAR EL OVERLAY
-          animationController.forward();
-        } // CUANDO SE PRESIONE EL ICONO, SE AGREGA/QUITA EL  MENU DEL OVERLAY
+          animationController.forward(); // ANIMACION DE APERTURA
+        } // CUANDO SE PRESIONE EL ICONO, SE AGREGA/QUITA EL modal
       },
     );
   }
 
   void _showOverlay() {
-    _overlayEntry = _createOverlayEntry(); // SE INICIALIZA EL OVERLAYENTRY
-    // SE INSERTA EL OVERLAY
-    Overlay.of(context)?.insert(_overlayEntry!);
-
-    setState(() {
-      isMenuVisible = true;
-    }); // SE ACUTALIZA EL ESTADO VISIBLE
-  } // METODO PARA MOSTRAR EL OVERLAY CON EL CubeTypeMenu
-
-  void _removeOverlay() {
-    _overlayEntry?.remove(); // SE ELIMINA EL OVERLAY
-    setState(() {
-      isMenuVisible = false;
-    }); // SE ACTUALIZA EL ESTADO VISIBLE
-  } // METODO PARA ELIMINAR EL OVERLAY
-
-  OverlayEntry _createOverlayEntry() {
-    return OverlayEntry(
-      builder: (context) {
-        return GestureDetector(
-          onTap: () {
-            // AL TOCAR FUERA, SE CIERRA EL MENU
-            _removeOverlay(); // ELIMINAR EL OVERLAY
-            animationController.reverse(); // VOLVER AL ESTADO INICIAL
-          },
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Container(
-                  // SE PONE EL FONDO MEDIO OSCURO CUANDO APARECE EL MENU
-                  color: Colors.black.withOpacity(0.4),
-                ),
-              ),
-              Center(
-                // ESTE CONTAINER SIRVE PARA CENTRARLO EN LA PANTALLA EL OTRO CONTAINER
-                child: Container(
-                  width: 250,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30)
-                  ),
-                  child: const CubeTypeMenu(), // MENÚ DE TIPOS DE CUBOS
-                ),
-              ),
-            ],
-          ),
+    showModalBottomSheet(
+      // backgroundColor: AppColors.purpleIntroColor,
+      context: context,
+      builder: (BuildContext context) {
+        return CubeTypeMenu(
+          onCubeTypeSelected: (selectedCubeType) {
+          }, // CUANDO SE SELECCIONE UN TIPO DE CUBO
         );
       },
-    );
-  } // CREAR EL OVERLAYENTRY PARA MOSTRAR EL CubeTypeMenu
+    ).whenComplete(() {
+      setState(() {
+        isMenuVisible = false;
+      });
+      animationController.reverse(); // REVERTIMOS LA ANIMACION DE CIERRE
+    }); // CUANDO EL MODAL SE HA CERRADO, REVERTIMOS LA ANIMACION
+
+    setState(() {
+      // CAMBIAMOS DE VALOR
+      isMenuVisible = true;
+    });
+  }
 
   @override
   void dispose() {
