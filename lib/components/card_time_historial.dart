cc27162627a47524b0ed6d3853ebd4214a80894e
estelaV9 <esteladeveg@gmail.com>
2025-01-29 09:49:08 +0100

    añadir una mediaQuery para el ancho de la pantalla y mostrar entre 3 a 5 cards de tiempos segun el tamaño de la pantalla

--- lib/components/card_time_historial.dart
+++ lib/components/card_time_historial.dart
@@ -30,28 +30,51 @@ class _CardTimeHistorialState extends State<CardTimeHistorial> {
 
   @override
   Widget build(BuildContext context) {
+    // USA MediaQuery PARA OBTENER EL ANCHO DE LA VENTANA
+    final screenWidth = MediaQuery.sizeOf(context).width;
+
+    // DEFINE LA CANTIDAD DE COLUMNAS BASADA EN EL ANCHO DE LA VENTANA
+    int crossAxisCount;
+    // TAMAÑO DE LA LETRA DEPENDIENTO EL TAMAÑO DE LA PANTALLA
+    double fontSize;
+    if (screenWidth < 300) {
+      crossAxisCount = 3; // TRES COLUMNA PARA MENOS DE 300PX
+      fontSize = 8;
+    } else if (screenWidth < 1000) {
+      crossAxisCount = 4; // CUATRO COLUMNAS ENTRE 300PX Y 1000PX
+      fontSize = 16;
+      if (screenWidth > 700) {
+        fontSize = 18;
+      } // SI LA PANTALLA ES MAYOR QUE 800, AUMENTA UN POCO EL TAMAÑO DE LA LETRA
+    } else {
+      crossAxisCount = 5; // CINCO COLUMNAS PARA MAS DE 1000PX
+      fontSize = 20;
+    }
+
     return GridView.builder(
       padding: const EdgeInsets.all(10),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
+        childAspectRatio: 1.4,
       ),
       itemCount: listTimes.length,
       itemBuilder: (context, index) {
         final time = listTimes[index];
         return SizedBox(
+          height: 50,
           child: Card(
             color: AppColors.lightVioletColor,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(10),
             ),
             elevation: 4,
             child: Center(
               child: Text(
-                "${time.timeInSeconds.toStringAsFixed(2)} s",
+                "${time.timeInSeconds.toStringAsFixed(2)}",
                 style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
               ),
             ),
           ),
