117186fd79d8c97842fbd87a3d2189dbb5216281
estelaV9 <esteladeveg@gmail.com>
2025-02-06 13:29:50 +0100

    modificar el provider de las estadisticas actuales y añadirle la lista de tiempos y metodos para settear todas las estadisticas

--- lib/viewmodel/current_statistics.dart
+++ lib/viewmodel/current_statistics.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/cupertino.dart';
+import '../data/dao/time_training_dao.dart';
+import '../model/time_training.dart';
 
 class CurrentStatistics extends ChangeNotifier {
-  String averageValue = "--:--.--";
-  String pbValue = "--:--.--";
-  String worstValue = "--:--.--";
-  String countValue = "--:--.--";
-  String ao5Value = "--:--.--";
-  String ao12Value = "--:--.--";
-  String ao50Value = "--:--.--";
-  String ao100Value = "--:--.--";
+  List<TimeTraining> _timesList = [];
 
-  void updateStatistics({
-    required String pb,
-    required String worst,
-    required String count,
-    String? average,
-    String? ao5,
-    String? ao12,
-    String? ao50,
-    String? ao100,
-  }) {
-    pbValue = pb;
-    worstValue = worst;
-    countValue = count;
-    if (average != null) averageValue = average;
-    if (ao5 != null) ao5Value = ao5;
-    if (ao12 != null) ao12Value = ao12;
-    if (ao50 != null) ao50Value = ao50;
-    if (ao100 != null) ao100Value = ao100;
+  List<TimeTraining> get timesList => _timesList;
+
+  void updateStatistics({required List<TimeTraining> timesListUpdate}) {
+    _timesList = timesListUpdate;
     notifyListeners();
-  } // METODO PARA ACTUALIZAR LAS ESTADISTICAS
+  } // ACTUALIZA LA LISTA DE TIEMPOS
+
+  Future<String> getPbValue() async {
+    return await TimeTrainingDao().getPbTimeBySession(_timesList);
+  } // OBTIENE EL PB DE TODOS LOS TIEMPOS DE LA SESION
+
+  Future<String> getWorstValue() async {
+    return await TimeTrainingDao().getWorstTimeBySession(_timesList);
+  } // OBTIENE EL PEOR TIEMPO DE TODOS LOS TIEMPOS DE LA SESION
+
+  Future<String> getCountValue() async {
+    int count = await TimeTrainingDao().getCountBySession(_timesList);
+    print(count);
+    return count.toString();
+  } // METODO PARA OBTENER EL TOTAL DE TIEMPOS EN LA SESION
+
+
+  /*Future<String> getAoXValue(int numAvg) async {
+    return await TimeTrainingDao().getAoX(_timesList, numAvg);
+  } // METODO AUXILIAR OBTENER LA MEDIA DE LOS X TIEMPOS DE LA SESION
+
+  Future<String> getAo5Value() async {
+    return await getAoXValue(5);
+  } // OBTIENE LA MEDIA DE LOS 5 TIEMPOS MAS RECIENTES
+
+  Future<String> getAo12Value() async {
+    return await getAoXValue(12);
+  } // OBTENER LA MEDIA DE LOS 12 TIEMPOS MAS RECIENTES
+
+  Future<String> getAo50Value() async {
+    return await getAoXValue(50);
+  } // METODO PARA OBTENER LA MEDIA DE LOS 50 TIEMPOS MAS RECIENTES
+
+  Future<String> getAo100Value() async {
+    return await getAoXValue(100);
+  } // METODO PARA OBTENER LA MEDIA DE LOS 100 TIEMPOS MAS RECIENTES*/
 }
