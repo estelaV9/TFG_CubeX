824c03df8fb05902e4a855a6eead3f888853aa6b
estelaV9 <esteladeveg@gmail.com>
2025-02-06 13:28:59 +0100

    solucionar el error de cuando se pulsa una sesion se settea el tipo de cubo a '2x2x2' y añadir la lista de tiempos de la sesion actual para las estadisticas

--- lib/view/components/session_menu.dart
+++ lib/view/components/session_menu.dart
@@ -1,5 +1,6 @@
 import 'package:esteladevega_tfg_cubex/data/dao/cubetype_dao.dart';
 import 'package:esteladevega_tfg_cubex/data/dao/session_dao.dart';
+import 'package:esteladevega_tfg_cubex/data/dao/time_training_dao.dart';
 import 'package:esteladevega_tfg_cubex/data/dao/user_dao.dart';
 import 'package:esteladevega_tfg_cubex/model/cubetype.dart';
 import 'package:esteladevega_tfg_cubex/viewmodel/current_cube_type.dart';
@@ -11,7 +12,9 @@ import 'package:provider/provider.dart';
 import '../../data/database/database_helper.dart';
 import '../../model/session.dart';
 import '../../viewmodel/current_session.dart';
+import '../../viewmodel/current_statistics.dart';
 import '../../viewmodel/current_user.dart';
+import '../screen/timer_screen.dart';
 
 class SessionMenu extends StatefulWidget {
   // FUNCION PARA ENVIAR LA SESION SELECCIONADA AL COMPONENTE QUE CREA
@@ -32,6 +35,7 @@ class _SessionMenuState extends State<SessionMenu> {
   SessionDao sessionDao = SessionDao();
   UserDao userDao = UserDao();
   CubeTypeDao cubeTypeDao = CubeTypeDao();
+  TimeTrainingDao timeTrainingDao = TimeTrainingDao();
   String sessionName = "";
 
   Future<void> getSessionOfUser() async {
@@ -286,10 +290,15 @@ class _SessionMenuState extends State<SessionMenu> {
                             // SE ACTUALIZA EL ESTADO GLOBAL
                             currentSession.setSession(sessions[index]);
 
+                            // BUSCAMOS EL TIPO DE CUBO QUE YA ESTABA ESTABLECIDO
+                            final tipoCuboEstablecido = context.read<CurrentCubeType>().cubeType;
+                            final cubo = await cubeTypeDao.cubeTypeDefault(tipoCuboEstablecido!.cubeName);
+
                             // BUSCAMOS EL TIPO DE CUBO QUE TIENE ESA SESION
                             Session? sessionTipoActual =
-                              await sessionDao.getSessionByUserAndName(idUser, currentSession.session!.sessionName);
+                              await sessionDao.getSessionByUserCubeName(idUser, currentSession.session!.sessionName, cubo.idCube);
 
+                            // CUANDO SELECCIONE UNA SESION, SE BUSCA EL TIPO DE CUBO DE ESA SESION
                             // GUARDAR LOS DATOS DEL TIPO DE CUBO EN EL ESTADO GLOBAL
                             final currentCube = Provider.of<CurrentCubeType>(
                                 this.context,
@@ -303,6 +312,14 @@ class _SessionMenuState extends State<SessionMenu> {
                             } else{
                               DatabaseHelper.logger.e("No se encontro el tipo de cubo: ${cubeType.toString()}");
                             } // SE VERIFICA QUE SE HA RETORNADO EL TIPO DE CUBO CORRECTAMENTE
+
+                            var timesList = await timeTrainingDao.getTimesOfSession(sessionTipoActual!.idSession);
+
+                            // GUARDAR LOS DATOS DE LAS ESTADISTICAS EN EL ESTADO GLOBAL
+                            final currentStatistics = Provider.of<CurrentStatistics>(
+                                this.context, listen: false);
+                            // SE ACTUALIZA EL ESTADO GLOBAL
+                            currentStatistics.updateStatistics(timesListUpdate: timesList);
 
 
                             setState(() {
