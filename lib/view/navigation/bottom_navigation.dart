d6e3e301e2073204d8a15961cccf322e304a1750
estelaV9 <esteladeveg@gmail.com>
2025-04-30 19:19:20 +0200

    integrar DoubleTapExitDialog y cargar estado global de user, session y cubeType al iniciar

--- lib/view/navigation/bottom_navigation.dart
+++ lib/view/navigation/bottom_navigation.dart
@@ -1,7 +1,16 @@
 import 'package:esteladevega_tfg_cubex/view/screen/timer_screen.dart';
 import 'package:esteladevega_tfg_cubex/view/utilities/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../model/cubetype.dart';
+import '../../model/session.dart';
+import '../../model/user.dart';
+import '../../viewmodel/current_cube_type.dart';
+import '../../viewmodel/current_session.dart';
+import '../../viewmodel/current_user.dart';
+import '../components/double_tap_exit_dialog.dart';
 import '../screen/historial_screen.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
@@ -14,16 +23,22 @@ import '../screen/statistics_screen.dart';
 /// - Gestos de deslizamiento lateral gracias a la integración con `PageView`.
 ///
 /// La selección de un elemento en el menú o el gesto de deslizamiento actualiza el estado y
-/// muestra la pantalla correspondiente.
+/// muestra la pantalla correspondiente. Además, carga los del usuario, tipo de cubo y
+/// sesión al iniciarse si el usuario esta logeado y actualiza su estado global.
+///
+/// Parámetros:
+/// - [index]: Índice inicial que se desea mostrar al iniciar la pantalla.
 class BottomNavigation extends StatefulWidget {
-  const BottomNavigation({super.key});
+  final int? index;
+
+  const BottomNavigation({super.key, this.index});
 
   @override
   State<BottomNavigation> createState() => _BottomNavigationState();
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  int _currentIndex = 1; // INDICE ACTUAL DEL BOTTOM NAVIGATION BAR
+  late int _currentIndex; // INDICE ACTUAL DEL BOTTOM NAVIGATION BAR
   late PageController _pageController = PageController(initialPage: _currentIndex);
 
   // LISTA DE WIDGETS PARA CADA PANTALLA
@@ -37,7 +52,36 @@ class _BottomNavigationState extends State<BottomNavigation> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    // SI SE HA PASADO UN INDICE SE ESTABLECE ESE, SI NO SE ESTABLECE EL TIMER
+    _currentIndex = widget.index ?? 1;
     _pageController = PageController(initialPage: _currentIndex);
+
+    // MANEJAR LOS DATOS SI EL USUARIO SE MANTIENE LOGEADO
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      // SE OBTIENE LAS SHAREDPERFERENDCES GUARDADAS
+      final prefs = await SharedPreferences.getInstance();
+
+      // CARGA LOS DATOS DEL USUARIO DESDE LAS PREFERENCIAS
+      User user = User.loadFromPreferences(prefs);
+      if (user.username != "") {
+        final currentUser = Provider.of<CurrentUser>(this.context, listen: false);
+        currentUser.setUser(user); // SE ACTUALIZA EL ESTADO GLOBAL
+      } // SE VERIFICA SI HAY UN USUARIO GUARDADO
+
+      // CARGA EL TIPO DE CUBO ACTUAL DESDE LAS PREFERENCIAS
+      CubeType cubeType = CubeType.loadFromPreferences(prefs);
+      if (cubeType.idUser != -1 || cubeType.idCube != -1) {
+        final currentCubetype = Provider.of<CurrentCubeType>(context, listen: false);
+        currentCubetype.setCubeType(cubeType); // SE ACTUALIZA EL ESTADO GLOBAL
+      } // SE VERIFICA SI EXISTEN DATOS VALIDOS
+
+      // CARGA LA SESION ACTUAL DESDE LAS PREFERENCIAS
+      Session session = Session.loadFromPreferences(prefs);
+      if (session.idUser != -1 || session.idSession != -1) {
+        final currentSession = Provider.of<CurrentSession>(context, listen: false);
+        currentSession.setSession(session); // SE ACTUALIZA EL ESTADO GLOBAL
+      } // SE VERIFICA SI EXISTEN DATOS VALIDOS
+    });
   }
 
   void _onTap(int index) {
@@ -69,30 +113,34 @@ class _BottomNavigationState extends State<BottomNavigation> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      body: PageView(
-        controller: _pageController,
-        children: _screens,
-        onPageChanged: (index) {
-          setState(() => _currentIndex = index);
-        },
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        index: _currentIndex,
-        height: 60,
-        // FONDO DEL NAVIGATION BAR (el color del espacio que deja el elemento seleccionado)
-        backgroundColor: Colors.transparent,
-        color: AppColors.lightVioletColor,
-        // COLOR DE LA BARRA
-        animationDuration: const Duration(milliseconds: 350),
-        // ANIMACION
-        onTap: _onTap,
-        items: <Widget>[
-          iconItem(Icons.manage_search, 0),
-          iconItem(Icons.timer, 1),
-          iconItem(Icons.auto_graph, 2)
-        ],
+    // PARA LAS PANTALLAS PRINCIPALES QUE MANEJA EL BOTTOM NAVIGATION SE LES APLICA
+    // LA VERIFICACION DE SALIDA DE LA APP, Y ASI EVITAR SALIDAS ACCIDENTALES
+    return DoubleTapExitDialog(
+      child: Scaffold(
+        extendBody: true,
+        body: PageView(
+          controller: _pageController,
+          children: _screens,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          index: _currentIndex,
+          height: 60,
+          // FONDO DEL NAVIGATION BAR (el color del espacio que deja el elemento seleccionado)
+          backgroundColor: Colors.transparent,
+          color: AppColors.lightVioletColor,
+          // COLOR DE LA BARRA
+          animationDuration: const Duration(milliseconds: 350),
+          // ANIMACION
+          onTap: _onTap,
+          items: <Widget>[
+            iconItem(Icons.manage_search, 0),
+            iconItem(Icons.timer, 1),
+            iconItem(Icons.auto_graph, 2)
+          ],
+        ),
       ),
     );
   }
